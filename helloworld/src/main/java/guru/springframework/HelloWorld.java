b4b4515d8d5509680f8933fcb437a4bc74f0ea60
Cleverson Rocha <cleversoncr@gmail.com>
2024-07-19 22:39:15 -0300

    Guru.springframework

--- helloworld/src/main/java/guru/springframework/HelloWorld.java
+++ helloworld/src/main/java/guru/springframework/HelloWorld.java
@@ -1,9 +1,12 @@
 package guru.springframework;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class HelloWorld {
 
   public static void main(String[] args) {
     System.out.println("Hello World");
+    System.out.println(StringUtils.capitalize("hello world"));
   }
 
 }

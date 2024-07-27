3849a1ac010622eae6306458f5bc0a329268c864
Cleverson Rocha <cleversoncr@gmail.com>
2024-07-26 22:24:54 -0300

    Construtores

--- coderJava/src/classe/Data.java
+++ coderJava/src/classe/Data.java
@@ -6,6 +6,18 @@ public class Data {
   String mes;
   int ano;
 
+  Data() {
+    dia = 1;
+    mes = "Janeiro";
+    ano = 1970;
+  }
+
+  Data(int diaInicial, String mesInicial, int anoInicial) {
+    dia = diaInicial;
+    mes = mesInicial;
+    ano = anoInicial;
+  }
+
   String obterDataFormatada() {
     return String.format("Hoje é %d de %s de %d", dia, mes, ano);
   }
--- coderJava/src/classe/DataTeste.java
+++ coderJava/src/classe/DataTeste.java
@@ -5,14 +5,14 @@ public class DataTeste {
   public static void main(String[] args) {
 
     Data d1 = new Data();
-    d1.dia = 24;
-    d1.mes = "Julho";
-    d1.ano = 2024;
+//    d1.dia = 24;
+//    d1.mes = "Julho";
+//    d1.ano = 2024;
 
-    var d2 = new Data();
-    d2.dia = 11;
-    d2.mes = "Setembro";
-    d2.ano = 2001;
+    var d2 = new Data(31, "Dezembro", 2020);
+//    d2.dia = 11;
+//    d2.mes = "Setembro";
+//    d2.ano = 2001;
 
 //    System.out.printf("Hoje é %d de %s de %d\n", d1.dia, d1.mes, d1.ano);
 //    System.out.printf("No dia %d de %s de %d ocorreu o atendado terrorista nos EUA", d2.dia, d2.mes, d2.ano);

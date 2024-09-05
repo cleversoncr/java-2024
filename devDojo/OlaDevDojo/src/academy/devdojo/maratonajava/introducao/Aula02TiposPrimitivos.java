87123ac1348a0544fbd6ea89a1df56cc9056713e
Cleverson Rocha <81713293+cleversoncr@users.noreply.github.com>
2024-09-04 21:21:24 -0300

    Inicio do DevDojo

---
+++ devDojo/OlaDevDojo/src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.introducao;
+
+/**
+ * Nomenclaturas (convenção)<br><br>
+ * Classes => padrão camelcase, sendo que a primeira <b>sempre</b> será maiúscula.<br>
+ * Variáveis => padrão camelcase, mas a primeira <b>sempre</b> será minúscula.
+ */
+public class Aula02TiposPrimitivos {
+  public static void main(String[] args) {
+    // int, double, float, char, byte, short, long, boolean
+    int idade = 10;
+    System.out.println("A idade é " + idade + " anos.");
+  }
+}

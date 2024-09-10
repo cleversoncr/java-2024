a61507813274253b48e54761db8001b625501f11
Cleverson Rocha <81713293+cleversoncr@users.noreply.github.com>
2024-09-10 20:21:28 -0300

    13 - Primitivos - Strings

--- devDojo/OlaDevDojo/src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ devDojo/OlaDevDojo/src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -3,7 +3,8 @@ package academy.devdojo.maratonajava.introducao;
 /**
  * Nomenclaturas (convenção)<br><br>
  * Classes >>> padrão camelcase, sendo que a primeira <b>sempre</b> será maiúscula.<br>
- * Variáveis >>> padrão camelcase, mas a primeira <b>sempre</b> será minúscula.
+ * Variáveis >>> padrão camelcase, mas a primeira <b>sempre</b> será minúscula.<br>
+ * <b>String</b> não é um tipo primitivo, mas sim, uma CLASSE
  */
 public class Aula02TiposPrimitivos {
   public static void main(String[] args) {
@@ -11,18 +12,20 @@ public class Aula02TiposPrimitivos {
     int idade = 10;
     int idadeCasting = (int) 10000000000L;
     long numeroGrande = 100000L;
-    double salarioDouble = 2000.0D;
-    float salarioFloat = 2000.0F;
+    double salarioDouble = 2000.0;
+    float salarioFloat = 2500.0F;
     byte salarioByte = 127;
     short salarioShort = 32000;
     boolean verdadeiro = true;
     boolean falso = false;
     char caractere = '\u0041';
+    String nome = "Goku";
 
     System.out.println("A idade é " + idade + " anos.");
     System.out.println(verdadeiro);
     System.out.println("char " + caractere);
     System.out.println("float " + salarioFloat);
     System.out.println(idadeCasting);
+    System.out.println("Oi meu nome é " + nome);
   }
 }
